83a631b0a0ba7be7a0ff8b2bf06d953bdf7792b3
서동해 <kaze@aivar.kr>
2023-02-08 10:20:27 +0900

    .env try

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    MoviesModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,18 +6,25 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   Patch,
   Post,
   Query,
+  Res,
 } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('movies')
 export class MoviesController {
-  constructor(private readonly MoviesService: MoviesService) {}
+  constructor(
+    private readonly MoviesService: MoviesService,
+    private readonly configService: ConfigService,
+  ) {}
   @Get()
   getAll(): Movie[] {
+    const chatServerIP = this.configService.get<string>('CHAT_SERVER_IP');
     return this.MoviesService.getAll();
   }
 
@@ -25,6 +32,13 @@ export class MoviesController {
   getOne(@Param('id') movieId: number): Movie {
     // 원하는 parameter을 @Param 데코레이터를 이용해 받을 수 있음.
     return this.MoviesService.getOne(movieId);
+    // const result = {
+    //   id: 2,
+    //   title: '2000',
+    //   year: 2000,
+    //   genres: ['2000'],
+    // };
+    // return result;
   }
 
   @Post()
